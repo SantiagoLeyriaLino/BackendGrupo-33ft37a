844221423eb09685e7340bebdaabb3e05cbd5121
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-13 11:25:13 -0300

    arreglo modelo users

--- src/db/models/usersSchema.js
+++ src/db/models/usersSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const usersScheme = new mongoose.Schema({
+const usersScheme = new Schema({
     name:{
         type: String,
         required: true
