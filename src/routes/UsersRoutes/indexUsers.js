ee5cc28fd37d9394c4466435cf5db90ff12f17c1
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-14 15:44:24 -0300

    ruta put

--- src/routes/UsersRoutes/indexUsers.js
+++ src/routes/UsersRoutes/indexUsers.js
@@ -3,6 +3,7 @@ const getHandlerUsers = require('./handlers/getHandlerUsers')
 const postHandlerUsers = require('./handlers/postHandlerUsers')
 const getIdHandlerUser = require('./handlers/getIdHandlerUser')
 const loginHandler = require('./handlers/loginHandler')
+const putHandlerUser = require('./handlers/putHandlerUser')
 
 const users = Router()
 
@@ -11,6 +12,8 @@ users.get('/', getHandlerUsers)
 users.get('/login', loginHandler)
 
 users.get('/:id', getIdHandlerUser)
+
+users.put('/:id', putHandlerUser)
 
 users.post('/', postHandlerUsers)
 
