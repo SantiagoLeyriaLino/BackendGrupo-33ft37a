e436c31b398d2068b66bd8c7836adef09ca6f363
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-06-14 13:22:16 -0300

    manejo de archivos y hasheo de claves

--- src/routes/UsersRoutes/controllers/postControllerUsers.js
+++ src/routes/UsersRoutes/controllers/postControllerUsers.js
@@ -1,10 +1,15 @@
 const Users = require('../../../db/models/usersSchema')
+const bcrypt = require('bcryptjs')
 
-const postControllerUsers = async(data) =>{
+const postControllerUsers = async(data, firebaseUrls) =>{
+    const saltRounds = 8;
+    let password = await bcrypt.hash(data.password, saltRounds)
+    console.log(password)
     let user = {
         name: data.name,
         email: data.email,
-        password: data.password,
+        image: firebaseUrls,
+        password: password,
         postalCode: data.postalCode,
         city: data.city,
         country: data.country,
--- src/routes/UsersRoutes/indexUsers.js
+++ src/routes/UsersRoutes/indexUsers.js
@@ -1,10 +1,16 @@
 const {Router} = require('express')
 const getHandlerUsers = require('./handlers/getHandlerUsers')
 const postHandlerUsers = require('./handlers/postHandlerUsers')
+const getIdHandlerUser = require('./handlers/getIdHandlerUser')
+const loginHandler = require('./handlers/loginHandler')
 
 const users = Router()
 
 users.get('/', getHandlerUsers)
+
+users.get('/login', loginHandler)
+
+users.get('/:id', getIdHandlerUser)
 
 users.post('/', postHandlerUsers)
 
